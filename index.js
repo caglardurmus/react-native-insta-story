f80b3c9d56521ed07e638554f8d577d8580528bc
Ahmet Çağlar Durmuş <caglardurmus@bulutklinik.com>
2022-01-04 11:10:46 +0300

    Revert "index.js update"
    This reverts commit 8e7b01f2c8430d658abe65755b260ad4b2936cf5.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import {requireNativeComponent} from 'react-native';
 import Story from "./src/Story";
 
-const InstaStory = requireNativeComponent('InstaStory', Story);
+export const InstaStory = Story;
 
 export default InstaStory;
