6040bef697ca6fac34f047a7544365d342c5785d
Ahmet Çağlar Durmuş <caglardurmus@bulutklinik.com>
2022-01-06 10:48:54 +0300

    StoryCircleListView.js converted to hooks.

--- src/StoryCircleListView.js
+++ src/StoryCircleListView.js
@@ -1,51 +1,46 @@
-import React, {Component} from "react";
+import React from "react";
 import {View, FlatList} from "react-native";
 import StoryCircleListItem from "./StoryCircleListItem";
 
-class StoryCircleListView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+const StoryCircleListView = (props) => {
 
-    render() {
-        const {
-            data,
-            handleStoryItemPress,
-            unPressedBorderColor,
-            pressedBorderColor,
-            avatarSize,
-            showText,
-            textStyle
-        } = this.props;
+    const {
+        data,
+        handleStoryItemPress,
+        unPressedBorderColor,
+        pressedBorderColor,
+        avatarSize,
+        showText,
+        textStyle
+    } = props;
 
-        return (
-            <View>
-                <FlatList
-                    keyExtractor={(item, index) => index.toString()}
-                    data={data}
-                    horizontal
-                    style={{paddingLeft: 12}}
-                    showsVerticalScrollIndicator={false}
-                    showsHorizontalScrollIndicator={false}
-                    ListFooterComponent={<View style={{flex: 1, width: 8}}/>}
-                    renderItem={({item, index}) => (
-                        <StoryCircleListItem
-                            avatarSize={avatarSize}
-                            handleStoryItemPress={() =>
-                                handleStoryItemPress && handleStoryItemPress(item, index)
-                            }
-                            unPressedBorderColor={unPressedBorderColor}
-                            pressedBorderColor={pressedBorderColor}
-                            item={item}
-                            showText={showText}
-                            textStyle={textStyle}
-                        />
-                    )}
-                />
-            </View>
-        );
-    }
+    return (
+        <View>
+            <FlatList
+                keyExtractor={(item, index) => index.toString()}
+                data={data}
+                horizontal
+                style={{paddingLeft: 12}}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+                ListFooterComponent={<View style={{flex: 1, width: 8}}/>}
+                renderItem={({item, index}) => (
+                    <StoryCircleListItem
+                        avatarSize={avatarSize}
+                        handleStoryItemPress={() =>
+                            handleStoryItemPress && handleStoryItemPress(item, index)
+                        }
+                        unPressedBorderColor={unPressedBorderColor}
+                        pressedBorderColor={pressedBorderColor}
+                        item={item}
+                        showText={showText}
+                        textStyle={textStyle}
+                    />
+                )}
+            />
+        </View>
+    );
+
 }
 
 export default StoryCircleListView;
