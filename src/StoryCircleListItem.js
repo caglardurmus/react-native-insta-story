ebb184742e9dda9dfab575b36c7e629beda2f49c
Ahmet Çağlar Durmuş <caglardurmus@bulutklinik.com>
2022-01-06 10:55:30 +0300

    StoryCircleListItem.js converted to hooks.

--- src/StoryCircleListItem.js
+++ src/StoryCircleListItem.js
@@ -1,116 +1,108 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import {View, Image, TouchableOpacity, Text, StyleSheet, Platform} from "react-native";
+import {usePrevious} from "./helpers/StateHelpers";
 
-// Constants
 import DEFAULT_AVATAR from "./assets/images/no_avatar.png";
 
-// Components
-class StoryCircleListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isPressed: this.props?.item?.seen
-        };
-    }
+const StoryCircleListItem = (props) => {
 
-    // Component Functions
-    _handleItemPress = item => {
-        const {handleStoryItemPress} = this.props;
+    const {
+        item,
+        unPressedBorderColor,
+        pressedBorderColor,
+        avatarSize,
+        showText,
+        textStyle
+    } = props;
+
+    const [isPressed, setIsPressed] = useState(props?.item?.seen);
+
+    const prevSeen = usePrevious(props?.item?.seen);
+
+    useEffect(() => {
+        if (prevSeen != props?.item?.seen) {
+            setIsPressed(props?.item?.seen);
+        }
+
+    }, [props?.item?.seen]);
+
+    const _handleItemPress = item => {
+        const {handleStoryItemPress} = props;
 
         if (handleStoryItemPress) handleStoryItemPress(item);
 
-        this.setState({isPressed: true});
+        setIsPressed(true);
     };
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevProps.item.seen != this.props.item.seen) {
-            this.setState({isPressed: this.props.item.seen});
-        }
-    }
+    const size = avatarSize ?? 60;
 
-    render() {
-        const {
-            item,
-            unPressedBorderColor,
-            pressedBorderColor,
-            avatarSize,
-            showText,
-            textStyle
-        } = this.props;
-
-        const size = avatarSize ?? 60;
-
-        const {isPressed} = this.state;
-
-        return (
-            <View style={styles.container}>
-                <TouchableOpacity
-                    onPress={() => this._handleItemPress(item)}
-                    style={[
-                        styles.avatarWrapper,
-                        {
-                            height: size + 4,
-                            width: size + 4,
-                        },
-                        !isPressed
-                            ? {
-                                borderColor: unPressedBorderColor
-                                    ? unPressedBorderColor
-                                    : 'red'
-                            }
-                            : {
-                                borderColor: pressedBorderColor
-                                    ? pressedBorderColor
-                                    : 'grey'
-                            }
-                    ]}
-                >
-                    <Image
-                        style={{
-                            height: size,
-                            width: size,
-                            borderRadius: 100,
-                        }}
-                        source={{uri: item.user_image}}
-                        defaultSource={Platform.OS === 'ios' ? DEFAULT_AVATAR : null}
-                    />
-                </TouchableOpacity>
-                {showText &&
-                    <Text
-                        numberOfLines={1}
-                        ellipsizeMode={'tail'}
-                        style={{
-                            width: size + 4,
-                            ...styles.text,
-                            ...textStyle
-                        }}>{item.user_name}</Text>}
-            </View>
-        );
-    }
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                onPress={() => _handleItemPress(item)}
+                style={[
+                    styles.avatarWrapper,
+                    {
+                        height: size + 4,
+                        width: size + 4,
+                    },
+                    !isPressed
+                        ? {
+                            borderColor: unPressedBorderColor
+                                ? unPressedBorderColor
+                                : 'red'
+                        }
+                        : {
+                            borderColor: pressedBorderColor
+                                ? pressedBorderColor
+                                : 'grey'
+                        }
+                ]}
+            >
+                <Image
+                    style={{
+                        height: size,
+                        width: size,
+                        borderRadius: 100,
+                    }}
+                    source={{uri: item.user_image}}
+                    defaultSource={Platform.OS === 'ios' ? DEFAULT_AVATAR : null}
+                />
+            </TouchableOpacity>
+            {showText &&
+                <Text
+                    numberOfLines={1}
+                    ellipsizeMode={'tail'}
+                    style={{
+                        width: size + 4,
+                        ...styles.text,
+                        ...textStyle
+                    }}>{item.user_name}</Text>}
+        </View>
+    );
 }
 
 export default StoryCircleListItem;
 
-const
-    styles = StyleSheet.create({
-        container: {
-            marginVertical: 5,
-            marginRight: 10
-        },
-        avatarWrapper: {
-            borderWidth: 2,
-            justifyContent: "center",
-            alignItems: "center",
-            alignSelf: "center",
-            borderColor: 'red',
-            borderRadius: 100,
-            height: 64,
-            width: 64
-        },
-        text: {
-            marginTop: 3,
-            textAlign: "center",
-            alignItems: "center",
-            fontSize: 11
-        }
-    });
+const styles = StyleSheet.create({
+    container: {
+        marginVertical: 5,
+        marginRight: 10
+    },
+    avatarWrapper: {
+        borderWidth: 2,
+        justifyContent: "center",
+        alignItems: "center",
+        alignSelf: "center",
+        borderColor: 'red',
+        borderRadius: 100,
+        height: 64,
+        width: 64
+    },
+    text: {
+        marginTop: 3,
+        textAlign: "center",
+        alignItems: "center",
+        fontSize: 11
+    }
+});
