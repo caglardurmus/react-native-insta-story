cc9e083ae4a1a1eca9d6b951b0d654ea20e8375c
Ahmet Çağlar Durmuş <caglardurmus@bulutklinik.com>
2022-03-01 16:57:53 +0300

    example update

--- example/App.js
+++ example/App.js
@@ -2,85 +2,40 @@ import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import InstaStory from 'react-native-insta-story';
 
-const data = [
-    {
-        user_id: 1,
-        user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
-        user_name: "Ahmet Çağlar Durmuş",
-        stories: [
-            {
-                story_id: 1,
-                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 1 swiped'),
-            },
-            {
-                story_id: 2,
-                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 2 swiped'),
-            },
-            {
-                story_id: 3,
-                story_image: "https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 1 swiped'),
-            }]
-    },
-    {
-        user_id: 2,
-        user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-        user_name: "Test User",
-        stories: [
-            {
-                story_id: 1,
-                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 1 swiped'),
-            },
-            {
-                story_id: 2,
-                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 2 swiped'),
-            },
-            {
-                story_id: 3,
-                story_image: "https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 1 swiped'),
-            }]
-    },
-    {
-        user_id: 3,
-        user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-        user_name: "Test User",
-        stories: [
-            {
-                story_id: 1,
-                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 1 swiped'),
-            },
-            {
-                story_id: 2,
-                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 2 swiped'),
-            },
-            {
-                story_id: 3,
-                story_image: "https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg",
-                swipeText: 'Custom swipe text for this story',
-                onPress: () => console.log('story 1 swiped'),
-            }]
-    }];
-
 export default function App() {
+    function createData() {
+        const array = []
+
+        const userCount = 10;
+        const userStoryCount = 15;
+
+        for (let i = 1; i <= userCount; i++) {
+            const storyArray = [];
+            for (let k = 1; k <= userStoryCount; k++) {
+                storyArray.push(
+                    {
+                        story_id: i,
+                        story_image: "https://picsum.photos/500/800?random=" + Math.random(),
+                        swipeText: 'Custom swipe text for this story',
+                        onPress: () => console.log(`story ${i} swiped`),
+                    }
+                )
+            }
+
+            array.push({
+                user_id: i,
+                user_image: "https://picsum.photos/200/300?random=" + Math.random(),
+                user_name: "Test User " + i,
+                stories: storyArray
+            })
+        }
+        return array;
+    }
+
     return (
         <View style={styles.container}>
             <StatusBar style="auto"/>
-            <InstaStory data={data}
+            <InstaStory data={createData()}
                         duration={10}
                         customSwipeUpComponent={<View>
                             <Text>Swipe</Text>
