af437949b9bb30664d1c355fed72785f5139d126
Yusuf Usta <yusufusta@mobilevictory.com>
2022-07-08 20:52:00 +0300

    removed android & ios folder from example

--- example/App.js
+++ example/App.js
@@ -1,65 +1,100 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  useColorScheme,
-  View,
-  Text
-} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 import InstaStory from 'react-native-insta-story';
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? "#000" : "#fff",
-  };
-
-  const data = [
+const data = [
     {
-      user_id: 1,
-      user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
-      user_name: "Ahmet Çağlar Durmuş",
-      stories: [
-        {
-          story_id: 1,
-          story_image: "https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg",
-          swipeText: 'Custom swipe text for this story',
-          onPress: () => console.log('story 1 swiped'),
-        },
-        {
-          story_id: 2,
-          story_image: "https://image.freepik.com/free-vector/mobile-wallpaper-with-fluid-shapes_79603-601.jpg",
-        }]
+        user_id: 1,
+        user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+        user_name: "Ahmet Çağlar Durmuş",
+        stories: [
+            {
+                story_id: 1,
+                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            },
+            {
+                story_id: 2,
+                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 2 swiped'),
+            },
+            {
+                story_id: 3,
+                story_image: "https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            }]
     },
     {
-      user_id: 2,
-      user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-      user_name: "Test User",
-      stories: [
-        {
-          story_id: 1,
-          story_video: "https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4",
-          swipeText: 'Custom swipe text for this story',
-          onPress: () => console.log('story 1 swiped'),
-        }]
+        user_id: 2,
+        user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        user_name: "Test User",
+        stories: [
+            {
+                story_id: 1,
+                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            },
+            {
+                story_id: 2,
+                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 2 swiped'),
+            },
+            {
+                story_id: 3,
+                story_image: "https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            }]
+    },
+    {
+        user_id: 3,
+        user_image: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        user_name: "Test User",
+        stories: [
+            {
+                story_id: 1,
+                story_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            },
+            {
+                story_id: 2,
+                story_image: "https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 2 swiped'),
+            },
+            {
+                story_id: 3,
+                story_image: "https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg",
+                swipeText: 'Custom swipe text for this story',
+                onPress: () => console.log('story 1 swiped'),
+            }]
     }];
 
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto"/>
+            <InstaStory data={data}
+                        duration={10}
+                        customSwipeUpComponent={<View>
+                            <Text>Swipe</Text>
+                        </View>}
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <InstaStory data={data}
-        duration={10}
-        onStart={item => console.log(item)}
-        onClose={item => console.log('close: ', item)}
-        customSwipeUpComponent={<View>
-          <Text>Swipe</Text>
-        </View>}
-        style={{ marginTop: 30 }} />
+                        style={{marginTop: 30}}
+            />
+        </View>
+    );
+}
 
-    </SafeAreaView>
-  );
-};
-
-export default App;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+});
