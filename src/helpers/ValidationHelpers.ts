6afd0d4d9d7d7fff67d3419f6bf0eb3bce9d1830
IB3N <benpearce9@live.co.uk>
2022-11-11 11:16:59 +0000

    refactor(ts): convert helpers to typescript and add barrel export

--- src/helpers/ValidationHelpers.ts
+++ src/helpers/ValidationHelpers.ts
@@ -1,4 +1,4 @@
-export function isNullOrWhitespace(input) {
+export function isNullOrWhitespace(input: any) {
   if (typeof input === 'undefined' || input == null) return true;
 
   return input.toString().replace(/\s/g, '').length < 1;
